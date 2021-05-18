7468c32c8a829fcf6c22bf86fd67075f9fe61daa
Matteo Melli <matteom@ongres.com>
2021-05-18 13:40:11 +0200

    ci: use zfs to create kind cache

--- stackgres-k8s/ci/test/e2e-gitlab-functions.sh
+++ stackgres-k8s/ci/test/e2e-gitlab-functions.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 [ "$DEBUG" != true ] || set -x
+SHELL_XTRACE="$(! echo $- | grep -q x || echo "-x")"
 
 set -e
 
 cd "$(dirname "$0")/../../.."
+
 mkdir -p stackgres-k8s/ci/test/target
 TEMP_DIR="/tmp/$CI_PROJECT_ID"
 mkdir -p "$TEMP_DIR"
@@ -36,4 +38,3 @@ then
 fi
 EOF
 }
-
